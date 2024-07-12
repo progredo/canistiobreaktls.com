b7042367cff9384fe3fe1abd8280d2351d0f2143
rob salmond <rob@salmond.ca>
2024-07-12 14:56:40 -0400

    img update

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+convert "source_images/canistiobreaktls.com.png" -crop 820x300+0+60 assets/app-to-thirdparty.png
+convert "source_images/canistiobreaktls.com (1).png" -crop 820x300+0+60 assets/client-to-app.png
